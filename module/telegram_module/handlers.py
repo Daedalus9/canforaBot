5c3db54622716e49586835be41ead688ba3491e6
Daedalus9 <longo.luca94@hotmail.it>
2022-12-02 11:49:27 +0100

    feat: added getadmin command

--- module/telegram_module/handlers.py
+++ module/telegram_module/handlers.py
@@ -1,6 +1,8 @@
 from telegram.ext import CommandHandler
 from module.telegram_module.start import start
+from module.telegram_module.get_admin import get_admin
 
 def cHandlers(updater):
     dispatcher=updater.dispatcher
     dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('getadmin', get_admin))
