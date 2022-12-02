6e156b9bec4488790be0c001abc440d3e447328c
Daedalus9 <longo.luca94@hotmail.it>
2022-12-02 14:08:49 +0100

    chore: added check for getadmin command

--- module/telegram_module/get_admin.py
+++ module/telegram_module/get_admin.py
@@ -1,8 +1,19 @@
 import yaml
+from var.text import NO_ROLE
 
 def get_admin(update, context):
     chat_id=update.effective_chat.id
-    with open(r"./var/admin.yaml") as file:
-        data = yaml.full_load(file)
+    confirm = 0
+    with open(r'var/admin.yaml') as file1:
+        data = yaml.full_load(file1)
         for admin in data['admin']:
-            context.bot.send_message(chat_id, admin)
+            if admin.split(" ", 1)[1] == str(chat_id):
+                confirm = 1
+                break
+    if confirm==1:
+        with open(r"./var/admin.yaml") as file:
+            data = yaml.full_load(file)
+            for admin in data['admin']:
+                context.bot.send_message(chat_id, admin)
+    else:
+        context.bot.send_message(chat_id, NO_ROLE)
